626bdff1cfaf109a2f3c504d91816bf65dc4f774
Michael Calcinai <michael@calcin.ai>
2020-09-29 12:24:01 +1300

    Add symlink for actual filename too

--- generate-symlinks.py
+++ generate-symlinks.py
@@ -25,9 +25,26 @@ for type in mapping.getroot().findall('*'):
 
             os.symlink(source_filename, dest_filename)
 
+            # Light - Actual filename
+            source_filename = '../../' + input_dir + '/' + type.tag + '/.Light/' + channel.find('File').text
+            dest_filename = output_dir_light + '/' + channel.find('File').text.rstrip('.png').lower()
+            if os.path.islink(dest_filename):
+                os.remove(dest_filename)
+
+            os.symlink(source_filename, dest_filename)
+
+
             # Dark
             source_filename = '../../' + input_dir + '/' + type.tag + '/.Dark/' + channel.find('File').text
             dest_filename = output_dir_dark + '/' + item.attrib['Name'].lower()
+            if os.path.islink(dest_filename):
+                os.remove(dest_filename)
+
+            os.symlink(source_filename, dest_filename)
+
+            # Dark - Actual filename
+            source_filename = '../../' + input_dir + '/' + type.tag + '/.Dark/' + channel.find('File').text
+            dest_filename = output_dir_dark + '/' + channel.find('File').text.rstrip('.png').lower()
             if os.path.islink(dest_filename):
                 os.remove(dest_filename)
 
