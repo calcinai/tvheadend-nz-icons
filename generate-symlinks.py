77a70f72141eb8b7bfc949cd9fca939b816a6cc7
Michael Calcinai <michael@calcin.ai>
2020-09-29 12:04:47 +1300

    Add light/dark variantsa

--- generate-symlinks.py
+++ generate-symlinks.py
@@ -2,10 +2,14 @@ import os
 import xml.etree.ElementTree as ET
 
 input_dir = 'mediaportal-nz-logos'
-output_dir = 'icons'
+output_dir_light = 'icons/light'
+output_dir_dark = 'icons/dark'
 
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
+if not os.path.exists(output_dir_light):
+    os.mkdir(output_dir_light)
+
+if not os.path.exists(output_dir_dark):
+    os.mkdir(output_dir_dark)
 
 mapping = ET.parse(input_dir + '/LogoMapping.xml')
 
@@ -13,8 +17,15 @@ for type in mapping.getroot().findall('*'):
     for channel in type.findall('Channel'):
         for item in channel.findall('Item'):
 
-            source_filename = '../' + input_dir + '/' + type.tag + '/Simple200/' + channel.find('File').text
-            dest_filename = output_dir + '/' + item.attrib['Name'].lower()
+            # Light
+            source_filename = '../' + input_dir + '/' + type.tag + '/.Light/' + channel.find('File').text
+            dest_filename = output_dir_light + '/' + item.attrib['Name'].lower()
+            if os.path.islink(dest_filename):
+                os.remove(dest_filename)
+
+            # Dark
+            source_filename = '../' + input_dir + '/' + type.tag + '/.Dark/' + channel.find('File').text
+            dest_filename = output_dir_dark + '/' + item.attrib['Name'].lower()
             if os.path.islink(dest_filename):
                 os.remove(dest_filename)
 
